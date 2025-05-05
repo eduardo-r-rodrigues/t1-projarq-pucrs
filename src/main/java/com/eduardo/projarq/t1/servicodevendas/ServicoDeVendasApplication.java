8f98d59af2cf38b1a923960584e3c7b1e337bff5
Eduardo Rosa <eduardorosa10000@gmail.com>
2025-05-05 20:30:35 -0300

    Inicializacao do projeto

---
+++ src/main/java/com/eduardo/projarq/t1/servicodevendas/ServicoDeVendasApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.projarq.t1.ServicoDeVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoDeVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/projarq/t1/ServicoDeVendas/ServicoDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.projarq.t1.ServicoDeVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
