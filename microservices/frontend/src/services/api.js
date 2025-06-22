c70bf9621ce94bff26f8fd270e0e3a919af85663
Eduardo Rosa <eduardorosa10000@gmail.com>
2025-06-22 16:01:15 -0300

    pronto

--- microservices/frontend/src/services/api.js
+++ microservices/frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+// Use relative URL to work with React proxy
+const API_BASE_URL = '/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
